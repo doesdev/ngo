29b3e7c5fa54d00b46006e2bb9b8454fbd60786b
Andrew Carpenter <andrew@audioinhd.com>
2019-07-14 22:33:30 -0400

    Update deps, use mvt for tests, 2.6.1

--- test/test.js
+++ test/test.js
@@ -1,9 +1,9 @@
 'use strict'
 
-import path from 'path'
-import { remove } from 'fs-extra'
-import test from 'ava'
-import ngo from './../index'
+const test = require('mvt')
+const path = require('path')
+const { remove } = require('fs-extra')
+const ngo = require('./../index')
 const goBinPath = path.resolve(__dirname, '..', 'vendor', 'go')
 
 test.before(() => {
@@ -11,37 +11,37 @@ test.before(() => {
   return remove(goBinPath).then(finish).catch(finish)
 })
 
-test.serial(`go.env returns environment`, (assert) => {
+test(`go.env returns environment`, (assert) => {
   let go = ngo({ useLocal: true })
   assert.truthy(go.env.GOPATH)
   assert.truthy(go.env.GOROOT)
   assert.truthy(go.env.GOBIN)
 })
 
-test.serial(`go('version') returns version`, async (assert) => {
+test(`go('version') returns version`, async (assert) => {
   let go = ngo({ useLocal: true })
-  assert.regex((await go('version')).stdout, /^go version/)
+  assert.truthy((await go('version')).stdout.match(/^go version/))
 })
 
-test.serial(`go(['version']) returns version`, async (assert) => {
+test(`go(['version']) returns version`, async (assert) => {
   let go = ngo({ useLocal: true })
-  assert.regex((await go(['version'])).stdout, /^go version/)
+  assert.truthy((await go(['version'])).stdout.match(/^go version/))
 })
 
-test.serial(`go(['get', 'somepackage']) works`, async (assert) => {
+test(`go(['get', 'somepackage']) works`, async (assert) => {
   let go = ngo({ useLocal: true })
-  await assert.notThrowsAsync(go(['get', '-u', 'golang.org/x/lint/golint']))
+  await assert.notThrowsAsync(() => go(['get', '-u', 'golang.org/x/lint/golint']))
 })
 
-test.serial(`go.bin('golint')('errors.go') works`, async (assert) => {
+test(`go.bin('golint')('errors.go') works`, async (assert) => {
   let golint = ngo({ useLocal: true }).bin('golint')
   let goFile = path.join(__dirname, 'fixtures', 'errors.go')
   let anError = /var unexp should have name of the form/
   let results = (await golint(goFile)).stdout
-  assert.regex(results, anError)
+  assert.truthy(results.match(anError))
 })
 
-test.serial(`go.bin('golint')('good.go') works`, async (assert) => {
+test(`go.bin('golint')('good.go') works`, async (assert) => {
   let golint = ngo({ useLocal: true }).bin('golint')
   let goFile = path.join(__dirname, 'fixtures', 'good.go')
   let results = (await golint(goFile)).stdout
